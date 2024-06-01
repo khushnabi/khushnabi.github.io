189af117beed152bd14c25d307650a7904c7b9d9
khushnabi shaikh <khushnabi@djtcorp.in>
2024-06-01 14:06:55 +0530

    Updates

--- _next/static/nhjnWLEon46l2nSNYlLKz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/64576b8262a8957b4ffc.css","static/chunks/pages/index-8ff5f37dbe85e634b280.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
