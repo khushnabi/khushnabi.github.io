070f7b564c4c55dd05bd94f954b90c0af6596082
khushnabi <p.khushnabi@gmail.com>
2025-10-29 11:51:42 +0530

    Updates

---
+++ _next/static/INmvXKJBbcT8wmcJruuw4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/b78a0092c5805c7b.css","static/chunks/pages/index-3b67fdb5fe26cc0b.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
